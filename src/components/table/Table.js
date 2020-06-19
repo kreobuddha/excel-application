2f76384c20e3e324999aa5adfb957f7e306c32f8
Rustam Ishmukhametov <kreobuddha@gmail.com>
2020-06-19 18:39:45 +0700

    finish base table logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,14 +1,24 @@
 import {ExcelComponent} from '@core/ExcelComponent'
+import {$} from '@core/dom'
+
 import {createTable} from '@/components/table/table.template'
 import {resizeHandler} from '@/components/table/table.resize'
-import {shouldResize} from '@/components/table/table.functions'
+import {TableSelection} from '@/components/table/TableSelection'
+import {
+  shouldResize,
+  matrix,
+  isCell,
+  nextSelector
+} from '@/components/table/table.functions'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
   constructor($root, options) {
     super($root, {
-      listeners: ['mousedown']
+      name: 'Table',
+      listeners: ['mousedown', 'keydown', 'input'],
+      ...options
     })
   }
 
@@ -16,9 +26,71 @@ export class Table extends ExcelComponent {
     return createTable(100)
   }
 
+  prepare() {
+    console.log('prepare')
+    this.selection = new TableSelection()
+  }
+
+  init() {
+    super.init()
+
+    console.log('init')
+
+    const $cell = this.$root.find('[data-id="0:0"]')
+    this.selectCell($cell)
+
+    this.$on('formula:input', text => {
+      this.selection.currentCell.text(text)
+      console.log(text)
+    })
+
+    this.$on('formula:done', () => {
+      this.selection.currentCell.focus()
+    })
+  }
+
+  selectCell($cell) {
+    this.selection.select($cell)
+    this.$emit('table:select', $cell)
+  }
+
   onMousedown(event) {
     if (shouldResize(event)) {
       resizeHandler(this.$root, event)
+    } else if (isCell(event)) {
+      const $target = $(event.target)
+      if (event.shiftKey) {
+        const $cells = matrix($target, this.selection.currentCell)
+            .map(id => this.$root.find(`[data-id="${id}"]`))
+
+        this.selection.selectGroup($cells)
+      } else {
+        this.selection.select($target)
+      }
     }
+  }
+
+  onKeydown(event) {
+    const keys = [
+      'Enter',
+      'Tab',
+      'ArrowLeft',
+      'ArrowRight',
+      'ArrowUp',
+      'ArrowDown'
+    ]
+    const {key} = event
+
+    if (keys.includes(key) && !event.shiftKey) {
+      event.preventDefault()
+
+      const id = this.selection.currentCell.id(true)
+      const $next = this.$root.find(nextSelector(key, id))
+      this.selectCell($next)
+    }
+  }
+
+  onInput(event) {
+    this.$emit('table:input', $(event.target))
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -13,6 +13,17 @@ class Dom {
     return this.$el.outerHTML.trim()
   }
 
+  text(text) {
+    if (typeof text === 'string') {
+      this.$el.textContent = text
+      return this
+    }
+    if (this.$el.tagName.toLowerCase() === 'input') {
+      return this.$el.value.trim()
+    }
+    return this.$el.textContent.trim()
+  }
+
   clear() {
     this.html('')
     return this
@@ -26,8 +37,29 @@ class Dom {
     this.$el.removeEventListener(listener, callback)
   }
 
+  find(selector) {
+    return $(this.$el.querySelector(selector))
+  }
+
   get data() {
     return this.$el.dataset
+  }
+
+  id(parse) {
+    if (parse) {
+      const id = this.id()
+      const parsed = id.split(':')
+      return {
+        row: +parsed[0],
+        col: +parsed[1]
+      }
+    }
+    return this.data.id
+  }
+
+  focus() {
+    this.$el.focus()
+    return this
   }
 
   append(node) {
@@ -62,6 +94,16 @@ class Dom {
         .forEach(key => {
           this.$el.style[key] = styles[key]
         })
+  }
+
+  addClass(className) {
+    this.$el.classList.add(className)
+    return this
+  }
+
+  removeClass(className) {
+    this.$el.classList.remove(className)
+    return this
   }
 }
 
